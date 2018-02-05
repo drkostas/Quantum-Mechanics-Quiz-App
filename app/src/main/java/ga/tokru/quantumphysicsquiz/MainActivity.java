e0608a31d2a45c362266f465a71eebb8e57af18a
drkostas <delfinas7kostas@gmail.com>
2018-02-05 21:30:53 +0200

    Initial Commit

---
+++ app/src/main/java/ga/tokru/quantumphysicsquiz/MainActivity.java
@@ -0,0 +1,14 @@
+package ga.tokru.quantumphysicsquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Quiz: Quantum Mechanics");
+    }
+}
